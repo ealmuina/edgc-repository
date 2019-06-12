d5c67fb5baf317d57141c0302edadf3dc391a0dc
Eddy Almuiña <almuina.e@gmail.com>
2019-06-13 00:08:34 +0200

    Reduced all tasks parameters (2)

--- populate_db.py
+++ populate_db.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
             com_intensity=0,
             mem_intensity=0,
             kernel='kernel/dlkernel1.so',
-            parameters='10000:2:1:0:2.500000:1000',
+            parameters='5000:2:1:0:2.500000:1000',
             output='output/out-1.tar.gz',
             pack='scripts/pack-1.sh',
             unpack='scripts/unpack-1.sh'
@@ -20,7 +20,7 @@ if __name__ == '__main__':
             com_intensity=0,
             mem_intensity=0,
             kernel='kernel/dlkernel2.so',
-            parameters='10000:2:1:0:2.500000:1000',
+            parameters='5000:2:1:0:2.500000:1000',
             output='output/out-2.tar.gz',
             pack='scripts/pack-2.sh',
             unpack='scripts/unpack-2.sh'
@@ -30,7 +30,7 @@ if __name__ == '__main__':
             com_intensity=0,
             mem_intensity=0,
             kernel='kernel/dlkernel3.so',
-            parameters='10000:2:1:0:2.500000:500',
+            parameters='5000:2:1:0:2.500000:500',
             output='output/out-3.tar.gz',
             pack='scripts/pack-3.sh',
             unpack='scripts/unpack-3.sh'
